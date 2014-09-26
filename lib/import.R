3607abb5f6787744fc1c1428d2d0bc8d449e6027
Philipp Sehnert <philipp.sehnert@gmail.com>
2014-09-26 16:25:01 +0200

    start implementation of slim go annotations

--- lib/import.R
+++ lib/import.R
@@ -45,4 +45,80 @@ read.pfam2go <- function(file) {
   names(data) <- c("id","name","go_name","go_id")
   return(data)
 }
-
+#' read.go2slim
+#'
+#' wrapper for read pfam2go mapping files provided by GO Consortium
+#' 
+#'@param file           mapping input file
+#'
+#'@return data.frame
+#'@export
+#'
+read.go2slim <- function(file) {
+  
+  # read in mapping file in obo format 
+  obo <- readLines(file)
+  
+  ## empty list to store results
+  obo1 <- vector("list")
+  # counter for list elements
+  z <- 0
+  
+  ## get all terms from lines
+  n <- get.terms(obo)
+  if(length(n)==0) stop("No terms to parse")
+  # add last postion in obo file (+2 )
+  n <- c(n, length(obo)+2 )
+  
+  for(i in 1:(length(n) - 1) ){
+    ## iterate through the terms
+    if(obo[n[i]] == "[Term]"){
+      # extract the complete Term
+      x <- obo[(n[i] + 1):(n[i + 1] - 2)]
+      # ids always first, name second?
+      id <- get.ID(x)
+      name <- get.Name(x)
+      namespace <- get.Namespace(x)
+      ## get all tag-value pairs for a single stanza
+      p <- x[grep("^is_a|^relationship", x)]
+      ## check if missing = root
+      if(length(p) == 0){
+        z <- z + 1
+        obo1[[z]] <- c(id, name, namespace, "root", id)
+      } else {
+        ## may have 1 or more parents...
+        for(j in 1:length(p)){
+          ## ID prefixes are usually upper-case (GO), some lower and upper (FBbt, LiPrO),
+          ## and some with underscore (DC_CL)
+          # may have commments after ID
+          pid <- gsub("(.* )([[:alpha:]|\\_]+:[[:digit:]]+)(.*)", "\\2", p[j])
+          ## capture relationship type??
+          rship <- gsub("(.*)( [[:alpha:]|\\_]+:[[:digit:]]+)(.*)", "\\1", p[j])
+          rship <- gsub("relationship: ", "", rship)
+          rship <- gsub("is_a:", "is_a", rship)
+          z <- z + 1
+          obo1[[z]] <- c(id, name, namespace, rship, pid)
+        }
+      }
+    }
+  }
+  obo2 <- data.frame(do.call("rbind", obo1), stringsAsFactors = FALSE)
+  colnames(obo2) <- c("id", "name", "ontology", "related", "parent_id")
+  
+  linage <- lapply(1:nrow(obo2),function(x) get.linage(x,obo2))
+  linage <- do.call("rbind",linage)
+  
+  obo3 <- data.frame("id" = obo2$id,
+                     "name" = obo2$name,
+                     "ontology" = obo2$ontology,
+                     "linage" = linage)
+  # remove switching ontologies
+  obo3 <- obo3[-grep("NA", obo3$linage), ]
+  
+  
+  class(obo2)<-c("obo", "data.frame")
+  attr(obo2, "created") <- date
+  attr(obo2, "downloaded") <- Sys.Date()
+  print(paste("Loaded", length(unique(obo2$id)), "unique terms and", nrow(obo2), "rows"))
+  obo2
+}
---
+++ lib/utils.R
@@ -0,0 +1,69 @@
+get.terms <- function(lines) {
+  # find the structure of "[TERM]" in lines object
+  return(grep("^\\[", lines))
+}
+
+get.ID <- function(term) {
+  return(gsub("(id: )(.*)", "\\2", term[grep("^id:", term)]))
+}
+
+get.Name <- function(term) {
+  return(gsub("(name: )(.*)", "\\2",  term[grep("*name:", term)]))
+}
+
+get.Namespace <- function(term) {
+  return(gsub("(namespace: )(.*)", "\\2",  term[grep("*namespace:", term)]))
+}
+
+get.alternatives <- function(term) {
+  return(gsub("(alt_id: )(.*)", "\\2",  term[grep("alt_id:", term)]))
+}
+
+is.root <- function(parent) {
+  return(if(length(parent) == 0) TRUE else FALSE)
+}
+
+get.linage <- function(index, complete.obo) {
+  # extract actual row from data.frame
+  data <- complete.obo[index, ]
+  # add first element to linage
+  linage <- data$name
+  # get realtionship
+  rship <- data$related
+  # get ontology
+  onto <- data$ontology
+  # build up linage until element is root
+  while(rship != "root") {
+    # find the parent in data.frame
+    data <- data.frame[grep(data$parent_id, data.frame$id), ]
+    # if parent has different ontology abort and give empty linage
+    if (all(unique(data$ontology) != onto)) {
+      linage <- NA
+      break
+    }
+    # check if only one relationship in parent
+    if (length(unique(data$related)) == 1) rship <- unique(data$related)
+    # change 'occurs_in' and 'has_part' to normal 'is_a'
+    if(rship == "occurs_in") rship <- "is_a"
+    if(rship == "has_part") rship <- "is_a"
+    # if parent is root compress it without relationship
+    if (any(data$related == "root")) {
+      data <- unique(data)
+    } else {
+      # if not follow path of relationship
+      data <- unique(data[grep(rship, data$related), ])
+    }
+    # add parent to linage
+    linage <- append(linage, data$name)
+    # adjust relationship
+    rship <- data$related
+  }
+  # return linage as character string in reverse order
+  return(toString(rev(linage)))    
+}
+
+equalize.linage <- function(linage) {
+  data <- str_split(linage, ", ")
+  max(do.call(rbind,lapply(data, function(x) length(x))))
+  
+}
